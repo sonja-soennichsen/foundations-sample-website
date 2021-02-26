765c82c1f6df8f0274f7bdc7e22c83ae4363e012
Sonja Soennichsen <sonja.soennichsen@axelspringer.com>
2021-02-26 08:16:45 +0100

    Finishing up form

--- week1/website.py
+++ week1/website.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title="Studying at Code")
 
 
 @app.route('/first-page')
 def first_page():
-    return render_template('first-page.html', page_title="First Template")
+    return render_template('first-page.html', page_title="Admissions")
 
 
 @app.route('/second-page')
 def second_page():
-    return render_template('second-page.html', page_title="Second Template")
+    return render_template('second-page.html', page_title="Study Programs")
 
 # add additonal pages here using a similar format as above
 
