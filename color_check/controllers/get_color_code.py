23d85dd576d586a7f9839702dbafd2630a5a4c79
Sonja Soennichsen <sonja.soennichsen@axelspringer.com>
2021-03-02 16:38:48 +0100

    Matching Userinput with Colorname

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -1,16 +1,21 @@
-# This file should contain a function called get_color_code().
-# This function should take one argument, a color name,
+# -This file should contain a function called get_color_code().
+# -This function should take one argument, a color name,
 # and it should return one argument, the hex code of the color,
 # if that color exists in our data. If it does not exist, you should
 # raise and handle an error that helps both you as a developer,
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
 
+import json
+
+
 def get_color_code(color_name):
-    # this is where you should add your logic to check the color.
-    # Open the file at data/css-color-names.json, and return the hex code
-    # The file can be considered as JSON format, or as a Python dictionary.
-
-    hex_code = '#0000ff'
-
-    return hex_code
+    # reading the data from the file
+    # looping though it, comparing keys with color_name
+    color = str(color_name)
+    with open('color_check/data/css-color-names.json') as f:
+        data = json.load(f)
+        for key in data.keys():
+            if color == key:
+                hex = data[key]
+                return hex
--- color_check/website.py
+++ color_check/website.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from flask import render_template
-from color_check.controllers.get_color_code import get_color_code
+from flask import Flask, render_template, request
+from controllers.get_color_code import get_color_code
 app = Flask(__name__)
 
 
@@ -19,11 +18,10 @@ def show_color():
     # - check if the color exists in our list, return the hex code if it does
     # - render a new page which shows a square of that color and its name
     # - if the color doesn't exist, give the user a useful error message.
-    # - create a log.txt file which records (logs) the user requests. 
+    # - create a log.txt file which records (logs) the user requests.
 
-    user_submitted_string = 'blue'
+    user_submitted_string = request.form.get('color')
     color_hex_code = get_color_code(user_submitted_string)
-
     return render_template('color.html', page_title="Show Color",
                            color_hex_code=color_hex_code)
 
